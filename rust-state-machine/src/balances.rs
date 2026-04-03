070ea28d0da287d496047e22cf7f12c423332787
obynonwane <biostechnologyng@gmail.com>
2026-04-03 19:07:29 +0000

    working on the state machine test

--- rust-state-machine/src/balances.rs
+++ rust-state-machine/src/balances.rs
@@ -10,7 +10,23 @@ impl Pallet {
         }
     }
 
-    pub fn set_balance(&mut self, who: &String, amunt: u128) {}
+    pub fn set_balance(&mut self, who: &String, amunt: u128) {
+        self.balances.insert(who.clone(), amunt);
+    }
 
-    pub fn balance(&self, who: &String) -> u128 {}
+    pub fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn init_balances() {
+        let mut balances = super::Pallet::new();
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+    }
 }
