60db40cc93ae9e64480cf89f8f9547679adf2c96
obynonwane <biostechnologyng@gmail.com>
2026-04-03 17:25:09 +0000

    working on state machine

--- rust-state-machine/src/balances.rs
+++ rust-state-machine/src/balances.rs
@@ -1,3 +1,16 @@
+use std::collections::BTreeMap; // import the TreeMap struct from the std::collections module
 pub struct Pallet {
- 
+    pub balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+    pub fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
+        }
+    }
+
+    pub fn set_balance(&mut self, who: &String, amunt: u128) {}
+
+    pub fn balance(&self, who: &String) -> u128 {}
 }
