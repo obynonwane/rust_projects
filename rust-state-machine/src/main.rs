47b653658e321382d3f68d23c857f4014a53e036
obynonwane <biostechnologyng@gmail.com>
2026-04-09 12:01:08 +0000

    worked on main.rs

--- rust-state-machine/src/main.rs
+++ rust-state-machine/src/main.rs
@@ -1,6 +1,41 @@
-
-mod balances; // import the balances module
+mod balances;
 mod system;
+
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+}
+
+impl Runtime {
+    // Create a new instance of the main Runtime, by creating a new instance of each pallet.
+    fn new() -> Self {
+        Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut runtime = Runtime::new();
+    let alice = "alice".to_string();
+    let bob = "bob".to_string();
+    let charlie = "charlie".to_string();
+
+    runtime.balances.set_balance(&alice, 100);
+
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+    runtime.system.inc_nonce(&alice);
+    let _res = runtime
+        .balances
+        .transfer(alice.clone(), bob, 30)
+        .map_err(|e| eprintln!("{e}"));
+    runtime.system.inc_nonce(&alice);
+    let _res = runtime
+        .balances
+        .transfer(alice, charlie, 20)
+        .map_err(|e| eprintln!("{e}"));
 }
